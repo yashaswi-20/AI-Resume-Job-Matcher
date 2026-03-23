bec6860aac81739e768eb527f1a9d87712b5cdb4
yashaswi-20 <yashaswi9199@gmail.com>
2026-03-23 17:35:46 +0530

    security concerns fix regarding keys

--- backend/test_gemini.js
+++ backend/test_gemini.js
@@ -1,6 +1,8 @@
 import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyA-avt6RvLsJPBSC6n1jDUGnwFf8OVW8qY" });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function main() {
     const response = await ai.models.generateContent({
